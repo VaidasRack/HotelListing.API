da219b4f92d32f423080288b46db7186c93a6266
Vaidas <Vaidas@VAIDAS-IDEAPAD3>
2022-10-27 22:34:13 +0300

    REST API Development Best Practices. Automapper and Repositories

---
+++ HotelListing.API/Models/Country/BaseCountryDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+
+namespace HotelListing.API.Models.Country
+{
+    public class BaseCountryDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
